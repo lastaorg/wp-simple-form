527d28dbf173c6de6730ca181a755863a245ecd9
Basliel Selamu <baslielselamu2018@gmail.com>
2021-12-04 08:17:34 +0300

    fix

--- shega-tools.php
+++ shega-tools.php
@@ -73,7 +73,7 @@ if (!class_exists('WPSimpleForm')) {
 
             // Build the form
 ?>
-           
+
             <div class="container" style="margin: auto; width: 50%">
                 <h1 id="calc-header">VAT Calculator</h1>
                 <p id="calc-desc">
@@ -119,42 +119,32 @@ if (!class_exists('WPSimpleForm')) {
                             $("#taxError").show();
                             return;
                         }
-                        switch (grossSalary) {
-                            case grossSalary <= 600:
-                                rate = 0;
-                                deducatable = 0;
-                                break;
-                            case 600 < grossSalary && grossSalary <= 1650:
-                                rate = 0.1;
-                                deducatable = 60;
-                                break;
-                            case 1650 < grossSalary && grossSalary <= 3200:
-                                rate = 0.15;
-                                deducatable = 142.5;
-                                break;
-                            case 3200 < grossSalary && grossSalary <= 5250:
-                                rate = 0.20;
-                                deducatable = 302.50;
-                                break;
-                            case 5250 < grossSalary && grossSalary <= 7800:
-                                rate = 0.25;
-                                deducatable = 565.00;
-                                break;
 
-                            case 7800 < grossSalary && grossSalary <= 10900:
-                                rate = 0.30;
-                                deducatable = 955.00;
-                                break;
+                        if (grossSalary <= 600) {
+                            rate = 0;
+                            deducatable = 0;
+                        } else if (600 < grossSalary && grossSalary <= 1650) {
+                            rate = 0.1;
+                            deducatable = 60;
+                        } else if (1650 < grossSalary && grossSalary <= 3200) {
+                            rate = 0.15;
+                            deducatable = 142.5;
+                        } else if (3200 < grossSalary && grossSalary <= 5250) {
+                            rate = 0.20;
+                            deducatable = 302.50;
+                        } else if (5250 < grossSalary && grossSalary <= 7800) {
+                            rate = 0.25;
+                            deducatable = 565.00;
+                        } else if (7800 < grossSalary && grossSalary <= 10900) {
+                            rate = 0.30;
+                            deducatable = 955.00;
+                            break;
+                        } else if (10900 < grossSalary) {
+                            rate = 0.35;
+                            deducatable = 1500.00;
+                        } else
+                            console.log("noting matxched", grossSalary, typeof grossSalary);
 
-                            case 10900 < grossSalary:
-                                rate = 0.35;
-                                deducatable = 1500.00;
-                                break;
-
-                            default:
-                                console.log("noting matxched", grossSalary, typeof grossSalary);
-                                break;
-                        }
 
                         pention = pention_rate * grossSalary;
                         incomeTax = (rate * grossSalary) - deducatable;

a397bbef52f1d89a628c95e4139539a12e049746
Basliel Selamu <baslielselamu2018@gmail.com>
2021-12-04 08:27:47 +0300

    fix

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,4 @@
-jQuery(document).ready(function() {
+/* jQuery(document).ready(function() {
     (function($) {
         $("#submitTax").click(function() {
             $("#taxError").hide();
@@ -59,4 +59,4 @@ jQuery(document).ready(function() {
             $("#results").show();
         })
     })(jQuery)
-});
+}); */

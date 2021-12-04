bec531ef34b61b24bd644e34c84adfefddf1481a
Basliel Selamu <baslielselamu2018@gmail.com>
2021-12-04 08:25:00 +0300

    main

--- public/js/main.js
+++ public/js/main.js
@@ -1,69 +1,62 @@
 jQuery(document).ready(function() {
-(function($) {
-    $("#submitTax").click(function() {
-        $("#taxError").hide();
-        $("#results").hide();
-        let netSalary;
-        let grossSalary;
-        let pention;
-        let incomeTax;
-        let rate;
-        let deducatable;
-        let pention_rate = .07;
+    (function($) {
+        $("#submitTax").click(function() {
+            $("#taxError").hide();
+            $("#results").hide();
+            let netSalary;
+            let grossSalary;
+            let pention;
+            let incomeTax;
+            let rate;
+            let deducatable;
+            let pention_rate = .07;
 
-        grossSalary = $("#monthlySalary").val();
+            grossSalary = Number.parseFloat($("#monthlySalary").val());
 
-        if (!grossSalary || grossSalary == 0) {
+            if (!grossSalary || grossSalary == 0) {
 
-            $("#taxError").html("<small>Please Enter a  valid salary.</small>");
-            $("#taxError").show();
-            return;
-        }
-        switch (grossSalary) {
-            case grossSalary <= 600:
+                $("#taxError").html("<small>Please Enter a  valid salary.</small>");
+                $("#taxError").show();
+                return;
+            }
+
+            if (grossSalary <= 600) {
                 rate = 0;
                 deducatable = 0;
-                break;
-            case 600 < grossSalary && grossSalary <= 1650:
+            } else if (600 < grossSalary && grossSalary <= 1650) {
                 rate = 0.1;
                 deducatable = 60;
-                break;
-            case 1650 < grossSalary && grossSalary <= 3200:
+            } else if (1650 < grossSalary && grossSalary <= 3200) {
                 rate = 0.15;
                 deducatable = 142.5;
-                break;
-            case 3200 < grossSalary && grossSalary <= 5250:
+            } else if (3200 < grossSalary && grossSalary <= 5250) {
                 rate = 0.20;
                 deducatable = 302.50;
-                break;
-            case 5250 < grossSalary && grossSalary <= 7800:
+            } else if (5250 < grossSalary && grossSalary <= 7800) {
                 rate = 0.25;
                 deducatable = 565.00;
-                break;
-
-            case 7800 < grossSalary && grossSalary <= 10900:
+            } else if (7800 < grossSalary && grossSalary <= 10900) {
                 rate = 0.30;
                 deducatable = 955.00;
                 break;
-
-            case 10900 < grossSalary:
+            } else if (10900 < grossSalary) {
                 rate = 0.35;
                 deducatable = 1500.00;
-                break;
+            } else {
+                console.log("noting matxched", grossSalary, typeof grossSalary);
+            }
 
-            default:
-                break;
-        }
+            console.log("rate", rate, deducatable);
 
-        pention = pention_rate * grossSalary;
-        incomeTax = (rate * grossSalary) - deducatable;
-        netSalary = grossSalary - pention - incomeTax;
+            pention = pention_rate * grossSalary;
+            incomeTax = (rate * grossSalary) - deducatable;
+            netSalary = grossSalary - pention - incomeTax;
 
-        $("#pention").text(pention);
-        $("#incomeTax").text(incomeTax);
-        $("#netSalary").text(netSalary);
-        $("#grossSalary").text(grossSalary);
-        $("#results").show();
-    })
-})(jQuery);
+            $("#pention").text(pention);
+            $("#incomeTax").text(incomeTax);
+            $("#netSalary").text(netSalary);
+            $("#grossSalary").text(grossSalary);
+            $("#results").show();
+        })
+    })(jQuery)
 });
